3efe05852fbda82cdcdeb0e77dda191c47ec058d
allyssamfournier <69099919+allyssamfournier@users.noreply.github.com>
2020-08-02 23:50:13 -0400

    Changed Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My New App
                 </div>
 
                 <div class="links">
